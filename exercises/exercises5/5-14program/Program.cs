6c014217bd67313210482a1e58c2db4b040a5b33
JasonSJ <jasonjokumsen177@gmail.com>
2026-09-09 23:35:51 +0200

    add program for exercise 5.14

---
+++ exercises/exercises5/5-14program/Program.cs
@@ -0,0 +1,14 @@
+﻿double mon = 21.5;
+double tue = 23.7;
+double wed = 19.6;
+double thu = 22.5;
+double fri = 25.3;
+double sat = 21.7;
+double sun = 18.9;
+
+Console.WriteLine(tue-mon);
+Console.WriteLine(wed-tue);
+Console.WriteLine(thu-wed);
+Console.WriteLine(fri-thu);
+Console.WriteLine(sat-fri);
+Console.WriteLine(sun-sat);
