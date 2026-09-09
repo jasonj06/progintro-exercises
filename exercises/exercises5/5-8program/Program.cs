e2963f74caf7323f5e69a198a8495d9fac0c699b
JasonSJ <jasonjokumsen177@gmail.com>
2026-09-09 22:43:22 +0200

    add program to calculate circle area

--- exercises/exercises5/5-8program/Program.cs
+++ exercises/exercises5/5-8program/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿byte circle1 = 1;   
+byte circle2 = 3;   // 3 circles with assigned radius   
+byte circle3 = 5;    
+
+static double area(byte radius)
+{
+    return 3.14*(radius*radius); // Returns calculation of area of circle with given radius (pi*r^2)
+}
+
+Console.WriteLine("Circle1 has area of: " + area(circle1));
+Console.WriteLine("Circle2 has area of: " + area(circle2));
+Console.WriteLine("Circle3 has area of: " + area(circle3));
