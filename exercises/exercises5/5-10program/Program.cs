0702a2f722c2fd97bd90531c729296ab45b2a17b
JasonSJ <jasonjokumsen177@gmail.com>
2026-09-09 23:03:29 +0200

    write program for calculation

--- exercises/exercises5/5-10program/Program.cs
+++ exercises/exercises5/5-10program/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿sbyte celsius; // Signed byte because temperature can be negative (Only accepts whole numbers)
+celsius = 15;
+
+static double celToFahr(sbyte c)
+{
+    return ( c * (9/5) ) + 32; // Formula for C to F: F = 32 + (9/5) * C
+}
+
+Console.WriteLine("15 degrees celsius is equal to " + celToFahr(celsius) + " degrees fahrenheit");
