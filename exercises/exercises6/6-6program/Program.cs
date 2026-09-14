05beac316bcbab405f0ac38d1b487e612d52d781
JasonSJ <jasonjokumsen177@gmail.com>
2026-09-14 13:18:54 +0200

    make program for 6.6

--- exercises/exercises6/6-6program/Program.cs
+++ exercises/exercises6/6-6program/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//  Prints celsius and corresponding fahrenheit from -5 to 40 degrees celsius
+
+for (double i = -5; i<=40; i=i+0.5) {
+    double fahr = (i*(9.0/5.0))+32;
+    double cel = i;
+    Console.WriteLine("celsius " + cel + " fahrenheit " + fahr);
+}
