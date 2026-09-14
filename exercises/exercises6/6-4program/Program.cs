9c27216f31d6462c5c35282a52240fca65bc69ee
JasonSJ <jasonjokumsen177@gmail.com>
2026-09-14 20:33:36 +0200

    partial solution for exercise 6.4

--- exercises/exercises6/6-4program/Program.cs
+++ exercises/exercises6/6-4program/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿byte monthNum = 9;
+
+if (monthNum%2 != 0 && monthNum<8) {    // Checks if month is odd or even, alternates at month 8,
+    Console.WriteLine("31 days");       // does not take care of february, and can go beyond 12.
+}
+else if (monthNum%2 == 0 && monthNum<8) {
+    Console.WriteLine("30 days");
+}
+else if (monthNum%2 != 0 && monthNum>=8) {
+    Console.WriteLine("30 days");
+}
+else if (monthNum%2 == 0 && monthNum>=8) {
+    Console.WriteLine("31 days");
+}
+
