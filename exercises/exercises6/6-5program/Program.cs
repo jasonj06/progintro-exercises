a436e995dad18b05b5a3a3fec3fe123adc293870
JasonSJ <jasonjokumsen177@gmail.com>
2026-09-14 13:01:25 +0200

    make program for 6.5

--- exercises/exercises6/6-5program/Program.cs
+++ exercises/exercises6/6-5program/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int month = 7;
+
+switch(month)
+{
+    case 4:
+        Console.WriteLine("Spring holiday");
+        break;
+    case 7:
+        Console.WriteLine("Summer holiday");
+        break;
+    case 8:
+        Console.WriteLine("Summer holiday");
+        break;
+    case 10:
+        Console.WriteLine("Autumn holiday");
+        break;
+    case 12:
+        Console.WriteLine("Christmas holiday");
+        break;
+    default:
+        Console.WriteLine("Hard work");
+        break;
+}
