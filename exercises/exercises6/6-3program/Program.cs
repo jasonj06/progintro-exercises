a0b467b3df9ea48e640a183efdbf65b36b76414a
JasonSJ <jasonjokumsen177@gmail.com>
2026-09-14 12:40:52 +0200

    Program for exercise 6.3

---
+++ exercises/exercises6/6-3program/Program.cs
@@ -0,0 +1,10 @@
+﻿int secondsPassed = 21816000; // Not in december
+//int secondsPassed = 28512001; // In december
+double price = 599.95;
+
+// Determining if it is christmas (the month of december)
+bool isChristmas = secondsPassed >= 28512000;
+
+double newPrice = (isChristmas ? (price*0.7) : price);
+
+Console.WriteLine("Is december: " + isChristmas + " Price is: " + newPrice);
