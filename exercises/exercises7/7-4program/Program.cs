efa02350b05209c1834cc305820a7d56f3f0cea5
JasonSJ <jasonjokumsen177@gmail.com>
2026-09-17 12:45:24 +0200

    make program for 7.4

--- exercises/exercises7/7-4program/Program.cs
+++ exercises/exercises7/7-4program/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int[] numbers = [2, 5, 10, 14, 2, 91, 9, 8, 21, 64];
+
+int num = 0;
+for (int i=0; i<numbers.Length; i++) {
+    if (numbers[i]>num) {
+        num = numbers[i];
+    }
+}
+Console.WriteLine(num);
